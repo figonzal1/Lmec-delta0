c213e31f0501995106283c15bf8a30031d6d7354
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2024-07-20 23:04:47 -0400

    agregado formulario para secrets

--- src/components/CexSecretsForm.tsx
+++ src/components/CexSecretsForm.tsx
@@ -5,57 +5,179 @@ import {
   ModalBody,
   ModalFooter,
   Button,
+  Input,
 } from "@nextui-org/react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { TbEye, TbEyeClosed } from "react-icons/tb";
+
+type FormValues = {
+  apiKey: string;
+  apiSecret: string;
+  password: string;
+};
 
 const CexSecretsForm = ({
   isOpen,
   onOpenChange,
+  onClose,
 }: {
   isOpen: boolean;
   onOpenChange: () => void;
+  onClose: () => void;
 }) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormValues>();
+
+  const [visibilityStates, setVisibilityStates] = useState([
+    false,
+    false,
+    false,
+  ]);
+
+  const [testPassed, setTestPassed] = useState(false);
+
+  const onSubmit = handleSubmit((data) => {
+    console.log("Form submitted:", data);
+    onClose();
+  });
+
+  const toggleVisibility = (index: number) => {
+    setVisibilityStates((prevStates) => {
+      const newStates = [...prevStates];
+      newStates[index] = !newStates[index];
+      return newStates;
+    });
+  };
+
   return (
     <Modal
       isOpen={isOpen}
       onOpenChange={onOpenChange}
       isDismissable={false}
+      isKeyboardDismissDisabled={true}
+      backdrop="blur"
+      hideCloseButton={true}
     >
       <ModalContent>
-        {(onClose) => (
-          <>
-            <ModalHeader className="flex flex-col gap-1">
-              Modal Title
-            </ModalHeader>
-            <ModalBody>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                pulvinar risus non risus hendrerit venenatis. Pellentesque sit
-                amet hendrerit risus, sed porttitor quam.
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                pulvinar risus non risus hendrerit venenatis. Pellentesque sit
-                amet hendrerit risus, sed porttitor quam.
-              </p>
-              <p>
-                Magna exercitation reprehenderit magna aute tempor cupidatat
-                consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex
-                incididunt cillum quis. Velit duis sit officia eiusmod Lorem
-                aliqua enim laboris do dolor eiusmod. Et mollit incididunt nisi
-                consectetur esse laborum eiusmod pariatur proident Lorem eiusmod
-                et. Culpa deserunt nostrud ad veniam.
-              </p>
-            </ModalBody>
-            <ModalFooter>
-              <Button color="danger" variant="light" onPress={onClose}>
-                Close
-              </Button>
-              <Button color="primary" onPress={onClose}>
-                Action
-              </Button>
-            </ModalFooter>
-          </>
-        )}
+        <form onSubmit={onSubmit}>
+          <ModalHeader className="flex flex-col gap-1">
+            Integración Bitget
+          </ModalHeader>
+          <ModalBody>
+            <h2>¿Donde consigo estas crendenciales?</h2>
+            <Input
+              {...register("apiKey", { required: true })}
+              type={visibilityStates[0] ? "text" : "password"}
+              label="Clave API"
+              radius="lg"
+              isRequired
+              isInvalid={errors.apiKey ? true : false}
+              errorMessage={
+                errors.apiKey?.type === "required"
+                  ? "Este campo es requerido"
+                  : ""
+              }
+              endContent={
+                <button
+                  className="focus:outline-none"
+                  type="button"
+                  onClick={() => toggleVisibility(0)}
+                  aria-label="toggle password visibility"
+                >
+                  {visibilityStates[0] ? (
+                    <TbEyeClosed className="text-2xl text-default-400 pointer-events-none" />
+                  ) : (
+                    <TbEye className="text-2xl text-default-400 pointer-events-none" />
+                  )}
+                </button>
+              }
+            />
+            <Input
+              {...register("apiSecret", { required: true })}
+              type={visibilityStates[1] ? "text" : "password"}
+              label="Clave Secreta"
+              radius="lg"
+              isRequired
+              isInvalid={errors.apiSecret ? true : false}
+              errorMessage={
+                errors.apiSecret?.type === "required"
+                  ? "Este campo es requerido"
+                  : ""
+              }
+              endContent={
+                <button
+                  className="focus:outline-none"
+                  type="button"
+                  onClick={() => toggleVisibility(1)}
+                  aria-label="toggle password visibility"
+                >
+                  {visibilityStates[1] ? (
+                    <TbEyeClosed className="text-2xl text-default-400 pointer-events-none" />
+                  ) : (
+                    <TbEye className="text-2xl text-default-400 pointer-events-none" />
+                  )}
+                </button>
+              }
+            />
+
+            <Input
+              {...register("password", { required: true })}
+              type={visibilityStates[2] ? "text" : "password"}
+              label="Contraseña token (passphrase)"
+              radius="lg"
+              isRequired
+              isInvalid={errors.password ? true : false}
+              errorMessage={
+                errors.password?.type === "required"
+                  ? "Este campo es requerido"
+                  : ""
+              }
+              endContent={
+                <button
+                  className="focus:outline-none"
+                  type="button"
+                  onClick={() => toggleVisibility(2)}
+                  aria-label="toggle password visibility"
+                >
+                  {visibilityStates[2] ? (
+                    <TbEyeClosed className="text-2xl text-default-400 pointer-events-none" />
+                  ) : (
+                    <TbEye className="text-2xl text-default-400 pointer-events-none" />
+                  )}
+                </button>
+              }
+            />
+
+            <Button
+              color="success"
+              variant="flat"
+              isDisabled={
+                !watch("apiKey") || !watch("apiSecret") || !watch("password")
+              }
+              onPress={() => setTestPassed(true)}
+            >
+              Test connection
+            </Button>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              color="danger"
+              variant="light"
+              onPress={onClose}
+              isDisabled={!testPassed}
+            >
+              Cerrar
+            </Button>
+            <Button color="primary" type="submit" isDisabled={!testPassed}>
+              Guardar
+            </Button>
+          </ModalFooter>
+        </form>
       </ModalContent>
     </Modal>
   );
--- src/components/Cexcard.tsx
+++ src/components/Cexcard.tsx
@@ -1,100 +1,107 @@
-import { Button, Card, CardBody, Image } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  Image,
+  useDisclosure,
+} from "@nextui-org/react";
 import { useState } from "react";
 import { MdDone } from "react-icons/md";
 import { TbPlugConnected, TbPlugConnectedX, TbSettings } from "react-icons/tb";
+import CexSecretsForm from "./CexSecretsForm";
 
 const Cexcard = ({
   name,
   description,
   logo,
-  onConnected,
 }: {
   name: string;
   description: string;
   logo: string;
-  onConnected: () => void;
 }) => {
   const [btnHover, setBtnHover] = useState(false);
   const [connected, setConnected] = useState(true);
 
+  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
+
   return (
-    <Card className="max-w-80" shadow="sm" radius="lg">
-      <CardBody className="p-5">
-        <div className="flex justify-between items-start">
-          <Image
-            alt={`${name} logo`}
-            height={48}
-            width={48}
-            radius="md"
-            src={logo}
-          />
-
-          {connected ? (
-            <>
-              <div className="flex items-center gap-2">
-                <Button
-                  radius="md"
-                  variant="flat"
-                  className="text-gray-400"
-                  onPress={() => {
-                    console.log("Settings button pressed");
-                  }}
-                  isIconOnly
-                >
-                  <TbSettings size={24} />
-                </Button>
-                <Button
-                  className="bg-green-300/45 text-green-600 hover:bg-red-300/45 hover:text-red-600"
-                  radius="md"
-                  variant="flat"
-                  onPress={() => {
-                    console.log("Settings button pressed");
-
-                    if (btnHover && connected) {
-                      console.log("Desconectando ...");
-                      setConnected(false);
-                    }
-                  }}
-                  onMouseEnter={() => {
-                    setBtnHover(true);
-                  }}
-                  onMouseLeave={() => {
-                    setBtnHover(false);
-                  }}
-                  startContent={
-                    <>
-                      {btnHover ? (
-                        <TbPlugConnectedX size={18} />
-                      ) : (
-                        <MdDone size={18} />
-                      )}
-                    </>
-                  }
-                >
-                  <h2 className="text-sm">
-                    {btnHover ? "Disconnect" : "Connected"}
-                  </h2>
-                </Button>
-              </div>
-            </>
-          ) : (
-            <Button
+    <>
+      <CexSecretsForm isOpen={isOpen} onOpenChange={onOpenChange} onClose={onClose} />
+      <Card className="max-w-80" shadow="sm" radius="lg">
+        <CardBody className="p-5">
+          <div className="flex justify-between items-start">
+            <Image
+              alt={`${name} logo`}
+              height={48}
+              width={48}
               radius="md"
-              variant="bordered"
-              onClick={() => {
-                //setConnected(true);
-                onConnected();
-              }}
-              startContent={<TbPlugConnected size={18} />}
-            >
-              <h2 className="text-sm">Connect</h2>
-            </Button>
-          )}
-        </div>
-        <h3 className="font-bold text-xl my-3">{name}</h3>
-        <p className="text-xs font-thin">{description}</p>
-      </CardBody>
-    </Card>
+              src={logo}
+            />
+
+            {connected ? (
+              <>
+                <div className="flex items-center gap-2">
+                  <Button
+                    radius="md"
+                    variant="flat"
+                    className="text-gray-400"
+                    onPress={() => {
+                      console.log("Settings button pressed");
+                    }}
+                    isIconOnly
+                  >
+                    <TbSettings size={24} />
+                  </Button>
+                  <Button
+                    className="bg-green-300/45 text-green-600 hover:bg-red-300/45 hover:text-red-600"
+                    radius="md"
+                    variant="flat"
+                    onPress={() => {
+                      console.log("Settings button pressed");
+
+                      if (btnHover && connected) {
+                        console.log("Desconectando ...");
+                        setConnected(false);
+                      }
+                    }}
+                    onMouseEnter={() => {
+                      setBtnHover(true);
+                    }}
+                    onMouseLeave={() => {
+                      setBtnHover(false);
+                    }}
+                    startContent={
+                      <>
+                        {btnHover ? (
+                          <TbPlugConnectedX size={18} />
+                        ) : (
+                          <MdDone size={18} />
+                        )}
+                      </>
+                    }
+                  >
+                    <h2 className="text-sm">
+                      {btnHover ? "Disconnect" : "Connected"}
+                    </h2>
+                  </Button>
+                </div>
+              </>
+            ) : (
+              <Button
+                radius="md"
+                variant="bordered"
+                onPress={onOpen}
+                startContent={<TbPlugConnected size={18} />}
+              >
+                <h2 className="text-sm">Connect</h2>
+              </Button>
+            )}
+          </div>
+          <h3 className="font-bold text-xl my-3">{name}</h3>
+          <p className="text-xs font-thin">{description}</p>
+        </CardBody>
+      </Card>
+    </>
   );
 };
 
